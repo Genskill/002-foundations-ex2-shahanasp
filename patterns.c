7fb575451c099b879850e1174973357e11adcd3b
SHAHANAS P <52955084+shahanasp@users.noreply.github.com>
2021-05-20 20:58:26 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,45 +1,40 @@
 #include<stdio.h>
-
 #include<cs50.h>
+void pyramid_f(int);
+void pyramid_s(int);
 
-void pyramid_one(int);
-void pyramid_two(int);
-
-void pyramid_one(int n) {
-  for (int i = n; i >= 1; i--) {
-    for (int j = i; j > 0; j--) {
-      printf("#");
-    }
-    printf("\n");
-  }
+int main(void){
+	int ch = get_int("Enter type of pattern (1 or 2) ");
+	if (ch == 1){
+		int row = get_int("Number of rows");
+		pyramid_f(row);
+	}
+	else{
+		if(ch == 2){
+			int row = get_int("Number of rows");
+			pyramid_s(row);
+			}
+		}
+	return 0;
 }
 
-void pyramid_two(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int k = n - i; k > 0; k--) {
-      printf(" ");
-    }
-    for (int j = 1; j <= i; j++) {
-      printf("#");
-    }
-    printf("\n");
-  }
+void pyramid_f(int n){
+	for(int i=n;i>=1;i--){
+		for(int j=i;j>=1;j--){
+			printf("#");
+		}
+		printf("\n");
+	}
 }
 
-int main(void) {
-  int ch = get_int(" ");
-  if (ch == 1) {
-    int row = get_int(" ");
-    printf("\n");
-    pyramid_one(row);
-  } else {
-    if (ch == 2) {
-
-      int row = get_int(" ");
-      pyramid_two(row);
-    }
-  }
-
-  return 0;
-
+void pyramid_s(int n){
+	for(int i=1;i<=n;i++){
+		for(int k=n-i;k>0;k--){
+			printf(" ");
+		}
+		for(int j=1;j<=i;j++){
+			printf("#");
+		}
+		printf("\n");
+	}
 }
